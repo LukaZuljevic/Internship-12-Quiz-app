a74319b26705c244326f598bc32a1e85445cd274
zulja1 <luka.zuljo@gmail.com>
2025-02-02 22:02:40 +0100

    Show questions and answers

--- modules/index.js
+++ modules/index.js
@@ -1,9 +1,10 @@
 import { fetchTriviaQuiz } from "./api.js";
 
+const form = document.querySelector("form");
 const sumbitButton = document.querySelector("#submit-button");
-
+const quizContainer = document.querySelector(".quiz-container");
 const startButton = document.createElement("button");
-startButton.textContent = "Start Quiz";
+const body = document.querySelector("body");
 
 sumbitButton.addEventListener("click", (event) => {
   event.preventDefault();
@@ -21,5 +22,50 @@ sumbitButton.addEventListener("click", (event) => {
 
   fetchTriviaQuiz(params).then((data) => {
     console.log(data);
+
+    if (data.length > 0) {
+      form.style.display = "none";
+      createStartButton(data);
+    }
   });
 });
+
+function createStartButton(data) {
+  startButton.textContent = "Start";
+  startButton.classList.add("start-button");
+  startButton.addEventListener("click", () => showQuiz(data));
+
+  body.appendChild(startButton);
+}
+
+function showQuiz(data) {
+  startButton.style.display = "none";
+  quizContainer.innerHTML = "";
+
+  data.forEach((item) => {
+    const question = document.createElement("h3");
+    question.textContent = item.question;
+    quizContainer.appendChild(question);
+
+    const answers = [...item.incorrect_answers, item.correct_answer].sort(
+      () => Math.random() - 0.5
+    );
+
+    answers.forEach((answer) => {
+      const answerBlock = document.createElement("p");
+      answerBlock.textContent = answer;
+
+      answerBlock.addEventListener("click", (event) =>
+        checkAnswer(data, event)
+      );
+
+      quizContainer.appendChild(answerBlock);
+    });
+
+    // quizContainer.innerHTML = "";
+  });
+}
+
+function checkAnswer(data, event) {
+  console.log(event.target);
+}
